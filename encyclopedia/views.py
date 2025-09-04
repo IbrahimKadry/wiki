30353d71aedbfe7ef66c9b72fd29b9a046438a32
HEMA <hemasasa47@gmail.com>
2025-09-04 19:02:16 +0200

    add some comments

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,13 +9,17 @@ def index(request):
     })
 
 def entry(request, title):
+    # هنا بيشوف لو في مقالة بنفس اسم المدخل 
     content = util.get_entry(title)
+
+    # لو مفيش مقالة بنفس الاسم بيرجع None 
     if content == None:
         return render(request, "encyclopedia/error.html",{
             "title": title,
             "message":"The requested page was not found."
         })
     
+    # هنا تحويل المحتوى الى لغة (اتش تي ام ال)
     html_content = markdown.markdown(content)
 
     return render(request, "encyclopedia/entry.html", {
@@ -31,12 +35,19 @@ def search(request):
         return redirect("index")
     # هنا بحفظ كل اسامي المقالات عندي في متغير 
     all_entries = util.list_entries()
-    
-    if query.lower() in [entry.lower() for entry in all_entries]:
-        return redirect("entry", title=query)
-    
-    results = [entry for entry in all_entries if query.lower() in entry.lower()]
 
+    # هنا بحول اي حرف  للاصغر عشان كله يبقا متشابه 
+    # ده شرط هيكون بصح او خطأ و بقول لو اللي بحثت عنه موجو بالظبط بنفس الحروف ولا لا 
+    if query.lower() in [entry.lower() for entry in all_entries]:
+
+        return redirect("entry", title=query)
+    # هنا تحويل للصفحة اللي فيها نتيجة البحث المتطابق تماما فقط
+
+
+    # هنا بجيب اي نتيجة تحتوي على حروف شبيهة للبحث و بحفهم في متغير واحد
+    results = [entry for entry in all_entries if query.lower() in entry.lower()]
+    
+    # هنا بيظهر كل النتائج في صفحة البحث على شكل لينكات
     return render(request, "encyclopedia/search.html",{
         "query": query,
         "results": results
