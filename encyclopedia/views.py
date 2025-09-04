793fed6cff13126cf0ffb0b012cbc1233166f319
HEMA <hemasasa47@gmail.com>
2025-09-04 06:56:20 +0200

    Add entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
 
 
@@ -8,3 +8,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content == None:
+        return render(request, "encyclopedia/error.html",{
+            "title": title,
+            "message":"The requested page was not found."
+        })
+    
+    html_content = markdown.markdown(content)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html_content
+    })
